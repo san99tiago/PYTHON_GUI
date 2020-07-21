5e347c52d0ede5d79f8c76a2ed19e125f6f96714
san99tiago <san99tiago@gmail.com>
2020-07-21 02:58:54 -0500

    Created first basic structure of a pyqt5 program

---
+++ PYQT/PYQT_1_BASICS/pyqt_1.py
@@ -0,0 +1,24 @@
+# BASICS OF PYQT4
+# Santiago Garcia Arango, July 2020
+# Tutorial based on <sentdex: PyQt Python GUI Application Development>
+
+"""
+Remark: these first scripts are over-commented, so that anyone can read them
+and get the basic understanding of the PyQt library.
+The are NOT intended to be totally efficient or following global standards.
+"""
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+# Create main app, where the parameter is by convention the argv connection
+app = QApplication(sys.argv)
+
+# Creation of main window, its geometry and the title
+window = QWidget()
+window.setGeometry(100, 100, 2560, 1440)  # May change based on screen-size
+window.setWindowTitle("San99tiago")
+
+# Always include the creation of the actual window and the exit method
+window.show()
+sys.exit(app.exec_())
